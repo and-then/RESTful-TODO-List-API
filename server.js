0e16a3a8ccb9d4fa81145dd250c96f7da0df55c9
xoak <54974852+xoak@users.noreply.github.com>
2019-11-20 14:44:22 -0800

    fixed typo, changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3003;
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'),
     bodyParser = require('body-parser');
@@ -8,7 +8,7 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb');
 
-app.arguments(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var routes = require('./api/routes/todoListRoutes');
